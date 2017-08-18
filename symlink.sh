aaaa5cd678779dd03829197c678b524f0a9f5646
ttoocs <ttoocs@gmail.com>
2017-08-18 14:58:57 -0600

    Changed how symlink script works.

--- symlink.sh
+++ symlink.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-if [ $1 == ""]; then
+if [ "$1" == "" ]; then
   echo "Symlinking based off of s2017 folder structure"
   PCL_DIR="../pcl"
 else
   PCL_DIR="$1"
 fi
 
-for i in ` find $PCL_DIR/gpu/kinfu_large_scale/* -type d -maxdepth 0` ; do
-  ln  -s $i ./
+
+COPYDIRS="include src test tools"
+KINFUDIR="gpu/kinfu_large_scale"
+#for i in ` find $PCL_DIR/gpu/kinfu_large_scale/* -type d -maxdepth 0` ; do
+rm -rf $COPYDIRS
+
+for i in $COPYDIRS; do
+  ln -s $PCL_DIR/$KINFUDIR/$i ./
 done
 
